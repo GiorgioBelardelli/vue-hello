1785dd0006017806eae25a9ba89de3ba79aeebd1
GiorgioBelardelli <asterapocalypse@gmail.com>
2023-12-07 16:44:46 +0100

    Fatto

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const { createApp } = Vue;
+
+createApp({
+    
+    data(){
+        return{
+            messaggio: "Io dopo l'esercizio di ieri:",
+            immagine:"img/walter-white-falling.gif"
+        }
+    }
+
+}).mount('#app')
